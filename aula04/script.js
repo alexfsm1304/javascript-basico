f62632b4324198f4152bd296b258efa47a1f4b96
Alex Fernando <alexfsmdevjava@gmail.com>
2023-01-05 15:35:09 -0300

    conteudo da aula 4 de javascript basico

---
+++ aula04/script.js
@@ -0,0 +1,36 @@
+// Comentário de uma linha
+
+/*
+   Comentário de multiplas linhas
+*/
+
+/*
+Um programa de computador é uma lista de "instruções" a serem "executadas" por um computador.
+
+Um programa JavaScript é uma lista de instruções de programação.
+
+Valores, operadores, expressões, palavras-chave e comentários.
+
+brek
+continue
+debugger
+do ... while
+for
+function
+if ...
+return
+switch
+try ...
+var
+let
+const
+
+*/
+
+alert('Aula 04 - JavaScript Básico') // Statement 1
+
+console.log('Hello World 1!') // Statement 2
+console.log('Hello World 2!') // Statement 3
+console.log('Hello World 3!') // Statement 4
+
+document.querySelector('#texto').innerText = 'Selecionando e alterando texto' // Statement 5
