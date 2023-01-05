06e371d7e96d0e73081fae8afe028fe07cb37591
Alex Fernando <alexfsmdevjava@gmail.com>
2023-01-05 15:12:15 -0300

    conteudo da aula 3 de javascript basico

---
+++ aula03/script.js
@@ -0,0 +1,8 @@
+alert('Hello World!')
+
+console.log('Hello World!')
+console.error('Exemplo de Erro!')
+
+document.write('Hello World')
+
+document.querySelector('#titulo').innerHTML = 'Alterei o Título'
