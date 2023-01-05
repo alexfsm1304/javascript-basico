ecf08410add89b425cd0795eb4b2e4df05207ee9
Alex Fernando <alexfsmdevjava@gmail.com>
2023-01-05 16:02:00 -0300

    conteudo da aula 5 de javascript basico

---
+++ aula05/script.js
@@ -0,0 +1,20 @@
+// var, let, const
+
+var nome = 'Alex' // string
+nome = 'Alex Fernando'
+var idade = 22 // number
+
+let altura = 1.82
+altura = 1.84
+
+const usuario = 'alexfsm1304'
+
+let nomeCompleto = 'Mickaela Rodrigues'
+
+console.log(nome)
+console.log(idade)
+console.log(altura)
+console.log(usuario)
+console.log(nomeCompleto)
+
+
